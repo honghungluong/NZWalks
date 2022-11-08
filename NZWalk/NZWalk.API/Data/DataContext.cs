3217bc12a045d30a548ade8a1b0120038c9a64e4
hunglh2 <luonghonghung95@gmail.com>
2022-11-08 13:02:04 +0700

    update EFCore feature

---
+++ NZWalk/NZWalk.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalk.API.Models.Domain;
+
+namespace NZWalk.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        // tellling EF that if a table doenst exist please create a table
+        DbSet<Region> Regions { get; set; }
+        DbSet<Walk> Walks { get; set; }
+        DbSet<WalkDiffculty> WalkDiffculties { get; set; }
+
+    }
+}
---
+++ NZWalk/NZWalk.API/Models/Domain/Region.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalk.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid RegionId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double lat { get; set; }
+        public double Long { get; set; }
+        public long Polulation { get; set; }
+
+        // collection navigation
+        // https://www.entityframeworktutorial.net/efcore/one-to-many-conventions-entity-framework-core.aspx?fbclid=IwAR1-oASBRxJE7YYZmrgvWqjXD8htluip_JGMdeciQB8wEDxpAVRuYNg266M
+        public IEnumerable<Walk> Walks;
+    }
+}
